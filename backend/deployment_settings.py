1bed9b1a34611e3948148033c47012d491ca98ac
damiraskme <119630528+damiraskme@users.noreply.github.com>
2025-06-10 21:42:55 -0400

    fixed secret_key

--- backend/deployment_settings.py
+++ backend/deployment_settings.py
@@ -21,10 +21,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3000",
-#     "http://127.0.0.1:3000"
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "https://portfolio-backend-azud.onrender.com"
+]
 
 STORAGES = {
   "default": {
